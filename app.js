461c161d19df7b7c6cd2df8411f49214bd512d91
Nazrul <nazrul.dev@gmail.com>
2023-05-10 20:59:50 +0800

    update

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const helmet = require('helmet');
-const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+
+import helmet from 'helmet';
+
+import 'dotenv/config'
+import express from 'express'
+import nodeCleanup from 'node-cleanup'
+import routes from './routes.js'
+import { init, cleanup } from './whatsapp.js'
+import cors from 'cors'
 
 const app = express();
-
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use('/', routes);
 
-// default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
-module.exports = app;
+
+var listener = app.listen(function(){
+    init()
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
+nodeCleanup(cleanup)
+
+app.all('*', (req, res) => res.status(404).json('route not defined'));
+
+export {app}
---
+++ routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import sessionsRoute from './routes/sessionsRoute.js'
+import chatsRoute from './routes/chatsRoute.js'
+import groupsRoute from './routes/groupsRoute.js'
+import response from './response.js'
+
+const router = Router()
+
+router.use('/sessions', sessionsRoute)
+router.use('/chats', chatsRoute)
+router.use('/groups', groupsRoute)
+router.all('/', (req, res) => {
+    res.json({messegae:"running"})
+})
+router.all('*', (req, res) => {
+    response(res, 403, false, 'The requested url cannot be found.')
+})
+
+export default router
